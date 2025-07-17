d6aaa791c00db68a6907149021735ca841d20673
desired23 <huydang2312003@gmail.com>
2025-07-17 20:53:58 +0700

    migrations

--- course/reviews2/views.py
+++
@@ -1,9 +0,0 @@
-# reviews/views.py
-from utils.decorators import role_required
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(["POST"])
-@role_required("student")
-def create_review(request):
-    return Response({"message": "Bạn đã gửi đánh giá thành công."})
